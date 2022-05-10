22b8075b9d9bf7449519143b2d294af41e62c676
Dev2 <Dev2@gmail.com>
2022-05-10 22:09:14 +0530

    First commit

---
+++ MathProj1/src/main/java/com/nt/App.java
@@ -0,0 +1,15 @@
+package com.nt;
+
+public class App {
+	public int add(int x,int y){
+	  return x+y;
+	}
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+	App app=new App();
+	int result=app.add(10,20);
+	System.out.println("Result" +result);
+	System.out.println("End of App");
+  }
+}
